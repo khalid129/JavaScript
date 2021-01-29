dc1771455c6f3dfc67787499f7c5739a33169fdf
Muhammad Khalid <muhammadkhalid.ch129@gmail.com>
2021-01-29 13:11:00 +0500

    JavaScript introduction and intervie question

---
+++ index.js
@@ -0,0 +1,35 @@
+// Variable in JavaScript
+// (Key) (Name) = (Value)
+// This is a pattern to declare a variable a in javascript
+
+// key in javascript are Var, Const ,let
+// var myName ="Khalid"
+// console.log(myName);
+
+// Data type in JavaScript
+
+// var value1 = 2;
+// var value2 = 10.5;
+// var value3 = true;
+// var value4 = null;  /*(It give data type object which is a bug)*/
+// var value5;
+// var myName = "Khalid";
+
+// console.log(typeof(value1));
+// console.log(typeof(value2));
+// console.log(typeof(value3));
+// console.log(typeof(value4));
+// console.log(typeof(value5));
+// console.log(typeof(myName));
+
+// NULL Vs Undefined
+
+// A variable is declare but not assign a value is called an undefined variable
+// A variable is daclare and assigned a value Null to it so it called Null variable
+
+// var value1 = null;
+// console.log(value1);
+
+// var value2 ;
+// console.log(value2);
+
