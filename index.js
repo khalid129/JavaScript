5843a36575efb996375dbfc57d24fcbbbbdd26bd
Muhammad Khalid <muhammadkhalid.ch129@gmail.com>
2021-01-29 13:43:34 +0500

    Operator, Operation and Expression in JavaScript

--- index.js
+++ index.js
@@ -33,3 +33,84 @@
 // var value2 ;
 // console.log(value2);
 
+// Expression and Operation
+
+// Expression is combination of Operand and an Operation
+
+// console.log(10+ "20"); 
+// When a Number and a string is Add so Console concatenate it no an Add;
+
+// console.log("Muhammad "+"Khalid");
+// when two string are add so it would be concetenate
+
+// console.log("20"-10);
+// When a Number string and a number would be subtract so it would be subtract which is a bugs in javascript
+
+// console.log("Khalid"-10);
+// console.log("Muhammad "-"Khalid");
+// When a string and number or a string and string would be subtracted so it would be give a NaN which is Not a Number
+
+// Use of NaN
+// NaN is global property of a Variable
+
+// var myName = "khalid";
+// var myPhoneNumber = 1234567890;
+
+// if(isNaN(myPhoneNumber)){
+//     console.log("please enter a number");
+// }
+// else{
+//     console.log("You enter a valid number");
+// }
+
+// Assignment Operator ( = )
+
+// var value1 = 10;
+// console.log(value1);
+
+// Increment ( ++ ) Decerement ( -- )  Operator
+
+// var num = 5;
+// console.log(num++);
+// Post Increment
+// console.log(num);
+
+// var num =5; 
+// console.log(++num);
+// Pre Incremnet
+// console.log(num);
+
+// var num = 5 ;
+// console.log(num--);
+// Post Decrement
+// console.log(num);
+
+// var num = 5;
+// console.log(--num);
+// Pre Decrement 
+// console.log(num);
+
+// Comparision Operator ( <=,>=,<,>,!=,==)
+
+var value1 = 10;
+var value2 = -10;
+
+// console.log(value1==value2);
+// Equality Operator
+
+// console.log(value1>value2);
+// Greater than operator
+
+// console.log(value1<value2);
+// Less than operator
+
+// console.log(value1<=value2);
+// Less than equalto Operator
+
+// console.log(value1>=value2);
+// Greater than equalto operator
+
+// console.log(value1!=value2);
+// Not Equalto operator
+
+
